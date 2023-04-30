ef17222010b37016e58ff102842b72a2e00a68f5
juan david calderon jimenez <j.calderonj@uniandes.edu.co>
2023-04-30 14:36:17 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'evaluacion_practica_MISW_4104_202312';
+}
